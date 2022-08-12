44562f3480809a01f9cad473bc6ef2e0d7aa2f56
Daniil Salyn <salyndaniil@gmail.com>
2022-08-13 02:52:29 +0300

    init client

---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './styles.module.scss'
+
+const HomePage = () => {
+  return (
+    <div className={styles.wrapper}>
+      home page
+    </div>
+  )
+}
+
+export default HomePage;
