9eddc13524711e0d531722a31c5f1974e17463a0
Daniil Salyn <salyndaniil@gmail.com>
2022-08-13 04:23:08 +0300

    add signup pagef

--- client/src/pages/SignUp/index.tsx
+++ client/src/pages/SignUp/index.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import styles from './styles.module.scss'
-import { Button } from '@mantine/core';
 
 const SignUp = () => {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      nickname: '',
+      password: '',
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+  });
   return (
-    <div>
-      signup
+    <div className={ styles.signUp }>
+      <Box className={ styles.signUp__form } >
+        <form onSubmit={ form.onSubmit((values) => console.log(values)) }>
+          <TextInput
+            label="Email"
+            placeholder="your@email.com"
+            { ...form.getInputProps('email') }
+          />
+          <TextInput
+            label="Username"
+            placeholder="your nickname"
+            { ...form.getInputProps('nickname') }
+          />
+          <TextInput
+            label="Password"
+            placeholder="password"
+            { ...form.getInputProps('password') }
+          />
+          <Group position="center" mt="md">
+            <Button className={ styles.signUp__submit } type="submit">Submit</Button>
+          </Group>
+        </form>
+      </Box>
     </div>
   )
 }
